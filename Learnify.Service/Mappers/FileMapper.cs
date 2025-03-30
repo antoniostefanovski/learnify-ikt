c79895eaca3f9addcf3dc0590ede4c1e19be5dca
ANTONIO\antoniostefanovski <antonio.stefanovski@yahoo.com>
2025-03-30 19:42:10 +0200

    Added file service

---
+++ Learnify.Service/Mappers/FileMapper.cs
@@ -0,0 +1,19 @@
+﻿using Learnify.Domain.DTO;
+using Learnify.Domain.Entities;
+
+namespace Learnify.Service.Mappers
+{
+    public static class FileMapper
+    {
+        public static Domain.Entities.File CopyFromDto(FileRequest fileRequest)
+        {
+            var file = new Domain.Entities.File();
+            file.FileName = fileRequest.FileName;
+            file.FileUrl = fileRequest.FileUrl;
+            file.LessonId = fileRequest.LessonId;
+            file.UploadedAt = DateTime.Now;
+
+            return file;
+        }
+    }
+}
