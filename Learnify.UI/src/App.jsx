e890960a983dba51330915f1ce23d0d799c83313
stefani2002 <eftimovskastefani8@gmail.com>
2025-04-04 18:13:15 +0200

    Course Description, Materials and Reviews (demo)

--- Learnify.UI/src/App.jsx
+++ Learnify.UI/src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css"; // Tailwind стилови
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";     // Login страница (фиктивна)
 import RegisterPage from "./components/RegisterPage"; // Register страница (фиктивна)
+import CoursePage, {Description,Materials,Reviews } from "./components/CoursePage"; // Course page (hardcoded)
 
 function App() {
     return (
@@ -12,6 +13,14 @@ function App() {
                 <Route path="/" element={<WelcomePage />} /> {/* Главната страница */}
                 <Route path="/login" element={<LoginPage />} /> {/* Login страница */}
                 <Route path="/register" element={<RegisterPage />} /> {/* Register страница */}
+
+                <Route path="/course" element={<CoursePage />}>
+                    <Route index element={<Description />} />
+                    <Route path="description" element={<Description />} />
+                    <Route path="materials" element={<Materials />} />
+                    <Route path="reviews" element={<Reviews />} />
+                </Route>
+
             </Routes>
         </Router>
     );
