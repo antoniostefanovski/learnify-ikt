f95ed8e11ac43ff40f06a4cd6a66e97f16297996
filipgjorgiev <filip.gjorgiev@outlook.finki.ukim.mk>
2025-04-11 18:39:49 +0200

    Created Review Service

---
+++ Learnify.Domain/DTO/UpdateReview.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnify.Domain.DTO
+{
+    public class UpdateReview
+    {
+        public Guid ReviewId { get; set; }
+        public double Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
+        public Guid CourseId { get; set; }
+        public DateTime ReviewdOn { get; set; }
+    }
+}
