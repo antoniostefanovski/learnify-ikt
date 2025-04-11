3247b369659b76dfcba7e91ed268b482e8bde6df
ANTONIO\antoniostefanovski <antonio.stefanovski@yahoo.com>
2025-04-11 18:41:21 +0200

    Implemented controllers for Answer / Enrollment / Lesson / Module entities

---
+++ Learnify.Web/Controllers/AnswerController.cs
@@ -0,0 +1,60 @@
+﻿using Learnify.Domain.DTO;
+using Learnify.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnify.Web.Controllers
+{
+    [Route("api/answers")]
+    [ApiController]
+    public class AnswerController : ControllerBase
+    {
+        private readonly IAnswerService answerService;
+
+        public AnswerController(IAnswerService answerService)
+        {
+            this.answerService = answerService;
+        }
+
+        [HttpGet("{questionId}")]
+        public IActionResult GetAnswers(Guid questionId)
+        {
+            var answers = answerService.GetAnswers(questionId);
+
+            return Ok(answers);
+        }
+
+        [HttpGet("{answerId}/get")]
+        public IActionResult GetAnswer(Guid answerId) 
+        {
+            var answer = answerService.GetAnswer(answerId);
+
+            return Ok(answer);
+        }
+
+        [HttpDelete("{answerId}")]
+        public IActionResult DeleteAnswer(Guid answerId) 
+        {
+            answerService.DeleteAnswer(answerId);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult InsertAnswer(AnswerRequest answerRequest)
+        {
+            answerService.InsertAnswer(answerRequest);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateAnswer(UpdateAnswer updateAnswer)
+        {
+            answerService.UpdateAnswer(updateAnswer);
+
+            return Ok();
+        }
+    }
+
+}
