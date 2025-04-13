ecb200f3e1dfd2a3362ab5cbac6f9eb8068cfee1
marijaaceska <marijatabule@gmail.com>
2025-04-13 11:09:18 +0200

    Added Leaderboard and Review controllers

---
+++ Learnify.Web/Controllers/ReviewController.cs
@@ -0,0 +1,60 @@
+﻿using Learnify.Domain.DTO;
+using Learnify.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnify.Web.Controllers
+{
+    [Route("api/reviews")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IReviewService reviewService;
+
+        public ReviewController(IReviewService reviewService)
+        {
+            this.reviewService = reviewService;
+        }
+
+        [HttpGet("course/{courseId}")]
+        public IActionResult GetReviews(Guid courseId)
+        {
+            var reviews = reviewService.GetReviewsByCourse(courseId);
+            
+            return Ok(reviews);
+
+        }
+
+        [HttpGet("{reviewId}")]
+        public IActionResult GetReview(Guid reviewId)
+        {
+            var review = reviewService.GetReview(reviewId);
+            
+            return Ok(review);
+        }
+
+        [HttpDelete("{reviewId}")]
+        public IActionResult DeleteReview(Guid reviewId)
+        {
+            reviewService.DeleteReview(reviewId);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateReview(UpdateReview updateReview)
+        {
+            reviewService.UpdateReview(updateReview);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult InsertReview(InsertReview insertReview)
+        {
+            reviewService.InsertReview(insertReview);
+
+            return Ok();
+        }
+
+    }
+}
